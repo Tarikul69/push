eedbb2c3a1633dc0ebba26a4cf43b0b0a0772787
tarikulabir <tarikulabir931@gmail.com>
2023-10-12 15:05:04 +0600

    Notifications

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,7 +9,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String notification = "Waitting for notification ";
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    //Terminated State
+    FirebaseMessaging.instance.getInitialMessage().then((event){
+      if (event != null) {
+        setState(() {
+        notification = "${event.notification!.title} ${event.notification!.body} I am comming from Terminated state";
+      }); 
+      }
+    });
+
+    //Forground State
+    FirebaseMessaging.onMessage.listen((event)
+    {
+      setState(() {
+        notification = "${event.notification!.title} ${event.notification!.body} I am comming from forground";
+      });
+    }); 
+
+    //Background State
+    FirebaseMessaging.onMessageOpenedApp.listen((event) { 
+      setState(() {
+        notification = "${event.notification!.title} ${event.notification!.body} I am comming from Background";
+      });
+    });
+
+    // 
+  }
+
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -16,7 +50,7 @@ class _HomePageState extends State<HomePage> {
       ),
       body:  Center(
         child: Text(
-          "Waiting for notification",
+          notification,
           textAlign: TextAlign.center,
         ),
       ),
