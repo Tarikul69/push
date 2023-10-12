269f37a2a3e7b00599109e890e4ce51ea5dc1959
tarikulabir <tarikulabir931@gmail.com>
2023-10-12 13:43:11 +0600

    Firebase update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:push/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_fire_cli/firebase_options.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
 );
